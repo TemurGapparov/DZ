2f5c245d1cef5a6c77f969ceb73aa740b738c5d1
Igor Dementev <analizatop69@gmail.com>
2023-08-04 17:15:15 +0300

    DZ3

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿            // *Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+// for (int count = 0; count == 0; ++count) // Пока ==, цикл работает.
+// {
+//     Console.Write("Введите 1-ое число: ");
+//     int input1 = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите 2-ое число: ");
+//     int input2 = Convert.ToInt32(Console.ReadLine());
+
+// if (input1 == input2)
+// {
+//     Console.WriteLine();
+//     Console.WriteLine("ВВЕДИТЕ РАЗНЫЕ ЧИСЛА");
+    
+//     count = -1;
+// }
+
+// if (input1 > input2)
+// {
+//     Console.WriteLine("Число " + input1 + " больше числа " + input2);
+// }
+
+// if (input1 < input2)
+// {
+//     Console.WriteLine("Число " + input2 + " больше числа " + input1);
+// }
+
+// }
+
+        // 2 ЗАДАНИЕ
+
+        
+            // *Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+// Console.Write("Введите 1-ое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 3-ое число: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Наибольшее число " + Math.Max(num1, Math.Max(num2, num3)));
+
+            // *Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка). 
+// Console.Write("Введите число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if (num1 % 2 == 0)
+// {
+//     Console.Write("Да");
+// }
+// else
+// {
+//     Console.Write("Нет");
+// }
+
+            // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
